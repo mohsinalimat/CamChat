d4777c70d3ac7793d3f85fe3db34ecb9a9bc54d1
Patrick Hanna <patrickjh1998@hotmail.com>
2018-10-21 23:26:34 -0400

    improvements

---
+++ HelpKit.framework/Headers/HelpKit.h
@@ -0,0 +1,21 @@
+//
+//  HelpKit.h
+//  HelpKit
+//
+//  Created by Patrick Hanna on 7/14/18.
+//  Copyright © 2018 Patrick Hanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+//! Project version number for HelpKit.
+FOUNDATION_EXPORT double HelpKitVersionNumber;
+
+//! Project version string for HelpKit.
+FOUNDATION_EXPORT const unsigned char HelpKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelpKit/PublicHeader.h>
+
+
